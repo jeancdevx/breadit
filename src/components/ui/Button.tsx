eba3ff39755e84c14d797943b205f1d277eaf30e
Jeancarlo Morales <jcode2006@gmail.com>
2023-07-30 09:55:39 -0500

    feat: :sparkles: add UI components from radix-ui

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,7 +1,7 @@
 import { cn } from '@/libs/utils'
 import { cva, type VariantProps } from 'class-variance-authority'
 import { Loader2 } from 'lucide-react'
-import * as React from 'react'
+import React from 'react'
 
 const buttonVariants = cva(
   'inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 active:scale-95 disabled:pointer-events-none disabled:opacity-50 dark:focus:ring-slate-400 dark:focus:ring-offset-slate-900',
@@ -10,8 +10,7 @@ const buttonVariants = cva(
       variant: {
         default: 'bg-zinc-900 text-zinc-100 hover:bg-zinc-800',
         destructive: 'text-white hover:bg-red-600 dark:hover:bg-red-600',
-        outline:
-          'bg-zinc-100 text-zinc-900 outline outline-1 outline-zinc-300 hover:bg-zinc-200',
+        outline: 'bg-zinc-100 text-zinc-900 hover:bg-zinc-200',
         subtle: 'bg-zinc-100 text-zinc-900 hover:bg-zinc-200',
         ghost:
           'bg-transparent text-zinc-800 hover:bg-zinc-100 data-[state=open]:bg-transparent',
---
+++ src/components/ui/Toaster.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import { useToast } from '@/hooks/use-toast.hook'
+import {
+  Toast,
+  ToastClose,
+  ToastDescription,
+  ToastProvider,
+  ToastTitle,
+  ToastViewport
+} from './Toast'
+
+export function Toaster() {
+  const { toasts } = useToast()
+
+  return (
+    <ToastProvider>
+      {toasts.map(function ({ id, title, description, action, ...props }) {
+        return (
+          <Toast
+            key={id}
+            {...props}
+          >
+            <div className='grid gap-1'>
+              {title !== undefined && <ToastTitle>{title}</ToastTitle>}
+              {description !== undefined && (
+                <ToastDescription>{description}</ToastDescription>
+              )}
+            </div>
+            {action}
+            <ToastClose />
+          </Toast>
+        )
+      })}
+      <ToastViewport />
+    </ToastProvider>
+  )
+}
