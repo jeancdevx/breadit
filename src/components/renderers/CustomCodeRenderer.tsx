3c7b93049fc66064633650ef1a3be5ea36a7b555
Jeancarlo Morales <jcode2006@gmail.com>
2023-07-30 09:58:13 -0500

    feat: :sparkles: add renderers components for editor

---
+++ src/components/renderers/CustomCodeRenderer.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+function CustomCodeRenderer({ data }: any) {
+  return (
+    <pre className='rounded-md bg-gray-800 p-4'>
+      <code className='text-sm text-gray-100'>{data.code}</code>
+    </pre>
+  )
+}
+
+export default CustomCodeRenderer
---
+++ src/components/renderers/CustomImageRenderer.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import Image from 'next/image'
+
+function CustomImageRenderer({ data }: any) {
+  const src = data.file.url
+
+  return (
+    <div className='relative min-h-[15rem] w-full'>
+      <Image
+        alt={data.file.name}
+        className='object-contain'
+        fill
+        src={src}
+      />
+    </div>
+  )
+}
+
+export default CustomImageRenderer
